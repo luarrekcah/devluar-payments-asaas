4d2434d14fd1e050d241ddfcc8f1621df7cceefb
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-04-08 16:47:11 -0500

    [added] route get prods

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,18 @@
+const { getAllItems } = require("../database");
+
 const express = require("express"),
   router = express.Router();
 
 router.get("/", (req, res, next) => {
-    res.sendStatus(200);
+  res.sendStatus(200);
+});
+
+router.get("/getproducts", async (req, res, next) => {
+  const allProds = await getAllItems({
+    path: "products"
+  });
+
+  res.json(allProds);
 });
 
 module.exports = router;
